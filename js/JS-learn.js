3242e513ccfa07fda200d789f9b3c6ee022d9e06
helloyppup <helloflrow@163.com>
2023-03-29 17:53:19 +0800

    3

--- js/JS-learn.js
+++ js/JS-learn.js
@@ -1,3 +1,19 @@
-let arr = [1, 2, 3, 99]
+let num = []
+for (let i = 0; i < 4; i++) {
+    num.push(prompt(`输入第${i + 1}季度的数据`))
+}
 
-console.log(arr.push(12))
+document.write(`
+<div class="box">
+`)
+for (let i = 0; i < num.length; i++) {
+    document.write(`
+        <div style="height: ${num[i]}px;">
+            <span> ${num[i]}</span>
+            <h4>第${i + 1}季度</h4>
+        </div>
+    `)
+}
+document.write(`
+</div>
+`)
